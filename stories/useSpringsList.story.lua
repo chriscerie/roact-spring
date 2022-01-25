7db1f3cb1a2e8814fc4abc231200cf370cbf848d
Christopher <51393127+chriscerie@users.noreply.github.com>
2022-01-24 22:55:28 -0800

    Pretty up useSpringsList story

--- stories/useSpringsList.story.lua
+++ stories/useSpringsList.story.lua
@@ -9,12 +9,41 @@ local RoactSpring = require(ReplicatedStorage.Packages.RoactSpring)
 
 local e = Roact.createElement
 
+local buttonProps = {
+    {
+        text = "LOREM",
+        gradient = ColorSequence.new({
+            ColorSequenceKeypoint.new(0, Color3.fromRGB(240, 215, 72)),
+            ColorSequenceKeypoint.new(1, Color3.fromRGB(236, 147, 57)),
+        }),
+    }, {
+        text = "IPSUM",
+        gradient = ColorSequence.new({
+            ColorSequenceKeypoint.new(0, Color3.fromRGB(241, 108, 230)),
+            ColorSequenceKeypoint.new(1, Color3.fromRGB(238, 71, 130)),
+        }),
+    }, {
+        text = "DOLOR",
+        gradient = ColorSequence.new({
+            ColorSequenceKeypoint.new(0, Color3.fromRGB(138, 223, 240)),
+            ColorSequenceKeypoint.new(1, Color3.fromRGB(172, 119, 224)),
+        }),
+    }, {
+        text = "SIT",
+        gradient = ColorSequence.new({
+            ColorSequenceKeypoint.new(0, Color3.fromRGB(177, 223, 233)),
+            ColorSequenceKeypoint.new(1, Color3.fromRGB(151, 182, 224)),
+        }),
+    }
+}
+
 local function Button(props, hooks)
     local springs, api = RoactSpring.useSprings(hooks, 5, function(i)
         return {
             from = {
-                Position = UDim2.fromScale(0.5, i * 0.2),
-                Size = UDim2.fromScale(1, 0.18),
+                Position = UDim2.fromScale(0.5, i * 0.16),
+                Size = UDim2.fromScale(1, 0.13), 
+                ZIndex = 1,
             },
         }
     end)
@@ -22,14 +51,14 @@ local function Button(props, hooks)
 
     local buttons = {}
 
-    for index = 1, 5 do
-        buttons[index] = e("TextButton", {
+    for index, buttonProp in ipairs(buttonProps) do
+        buttons[index] = e("ImageButton", {
             AnchorPoint = Vector2.new(0.5, 0.5),
             Position = springs[index].Position.value,
             Size = springs[index].Size.value,
-            BackgroundColor3 = Color3.fromRGB(99, 255, 130),
+            BackgroundColor3 = Color3.fromRGB(255, 255, 255),
             AutoButtonColor = false,
-            Text = "Click me",
+            ZIndex = springs[index].ZIndex.value,
 
             [Roact.Event.InputBegan] = function(button, input)
                 if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
@@ -42,17 +71,18 @@ local function Button(props, hooks)
                             if i == index then
                                 return {
                                     Position = UDim2.fromScale(0.5, (yPos - frame.AbsolutePosition.Y) / frame.AbsoluteSize.Y),
-                                }, {
-                                    immediate = true,
-                                }
+                                    ZIndex = 10,
+                                }, { immediate = true, }
                             end
-                            return {}
+                            return {
+                                ZIndex = 1,
+                            }, { immediate = true, }
                         end)
 
                         api.start(function(i)
                             if i == index then
                                 return {
-                                    Size = UDim2.new(1, 20, 0.18, 20),
+                                    Size = UDim2.new(1, 15, 0.13, 15),
                                 }, RoactSpring.constants.config.stiff
                             end
                             return {}
@@ -72,7 +102,7 @@ local function Button(props, hooks)
                         api.start(function(i)
                             if i ~= index then
                                 return {
-                                    Position = UDim2.fromScale(0.5, (table.find(sortedButtonNums, i) - 1) * 0.2 + 0.2),
+                                    Position = UDim2.fromScale(0.5, (table.find(sortedButtonNums, i) - 1) * 0.16 + 0.16),
                                 }
                             end
                             return {}
@@ -100,21 +130,49 @@ local function Button(props, hooks)
 
                     api.start(function(i)
                         return {
-                            Position = UDim2.fromScale(0.5, (table.find(sortedButtonNums, i) - 1) * 0.2 + 0.2),
-                            Size = UDim2.fromScale(1, 0.18),
+                            Position = UDim2.fromScale(0.5, (table.find(sortedButtonNums, i) - 1) * 0.16 + 0.16),
+                            Size = UDim2.fromScale(1, 0.13),
                         }
                     end)
                 end
             end
         }, {
             UICorner = e("UICorner"),
+            UIGradient = e("UIGradient", {
+                Color = buttonProp.gradient,
+            }),
+            UIStroke = e("UIStroke", {
+                ApplyStrokeMode = Enum.ApplyStrokeMode.Border,
+                Color = Color3.fromRGB(95, 95, 95),
+                Thickness = 2,
+                Transparency = 0,
+            }, {
+                UIGradient = e("UIGradient", {
+                    Rotation = -90,
+                    Transparency = NumberSequence.new({
+                        NumberSequenceKeypoint.new(0, 0.85),
+                        NumberSequenceKeypoint.new(1, 1),
+                    }),
+                }),
+            }),
+            Label = e("TextLabel", {
+                AnchorPoint = Vector2.new(0.5, 0.5),
+                Position = UDim2.fromScale(0.5, 0.5),
+                Size = UDim2.fromScale(0.7, 0.7),
+                BackgroundTransparency = 1,
+                TextColor3 = Color3.fromRGB(255, 255, 255),
+                Text = buttonProp.text,
+                TextSize = 22,
+                TextXAlignment = Enum.TextXAlignment.Left,
+                ZIndex = springs[index].ZIndex.value,
+            }),
         })
     end
 
 	return e("Frame", {
         AnchorPoint = Vector2.new(0.5, 0.5),
         Position = UDim2.fromScale(0.5, 0.5),
-		Size = UDim2.fromScale(0.4, 0.7),
+		Size = UDim2.fromScale(0.3, 0.7),
 		BackgroundTransparency = 1,
 	}, buttons)
 end
